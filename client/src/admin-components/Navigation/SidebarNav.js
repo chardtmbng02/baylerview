7f1a3edbc03058b95631e22c3bd8edf52dc8d053
Richard T <richard.tambong04@gmail.com>
2023-07-02 14:09:30 +0800

    fix sidebar

---
+++ client/src/admin-components/Navigation/SidebarNav.js
@@ -0,0 +1,126 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+export const SidebarNav = () => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
+  const handleLogout = () => {
+    sessionStorage.removeItem("session_id");
+  };
+
+  return (
+    <>
+      <nav className="flex-1 px-2 py-4 bg-gray-800">
+        <div className="mb-6 px-6 py-4">
+        <Link to="#"><span
+            className="inline-block text-white hover:text-red-500 my-2 w-full"
+          >
+            <span className="material-icons-outlined float-left pr-2">
+              dashboard
+            </span>
+            Dashboard
+          </span>
+          </Link>
+
+          <Link to="#"><span
+            className="inline-block text-white hover:text-red-500 my-2 w-full"
+          >
+            <span className="material-icons-outlined float-left pr-2">
+              book
+            </span>
+            Reservations
+          </span>
+          </Link>
+
+          <span className="text-gray-500 text-sm">Manage</span>
+          <span
+            className="inline-block text-white hover:text-red-500 my-2 w-full"
+            onClick={toggleDropdown}
+          >
+            <span className="material-icons-outlined float-left pr-2">bed</span>
+            Rooms
+            <span className="material-icons-outlined float-right">
+              {isDropdownOpen ? "keyboard_arrow_up" : "keyboard_arrow_down"}
+            </span>
+          </span>
+          {isDropdownOpen && (
+            <div className="pl-8">
+              <Link to="#"><span
+                className="inline-block text-white hover:text-red-500 my-2 w-full"
+              >
+                Standard
+              </span>
+              </Link>
+
+              <Link to="#"><span
+                className="inline-block text-white hover:text-red-500 my-2 w-full"
+              >
+                Deluxe
+              </span>
+              </Link>
+
+              <Link to="#"><span
+                className="inline-block text-white hover:text-red-500 my-2 w-full"
+              >
+                Sweet
+              </span>
+              </Link>
+            </div>
+          )}
+          <Link to="#"><span
+            className="inline-block text-white hover:text-red-500 my-2 w-full"
+          >
+            <span className="material-icons-outlined float-left pr-2">
+              chat
+            </span>
+            Feedbacks
+          </span>
+          </Link>
+          <Link to="#"><span className="inline-block text-white hover:text-red-500 my-2 w-full"
+          >
+            <span className="material-icons-outlined float-left pr-2">
+              record_voice_over
+            </span>
+            Testimonials
+          </span>
+          </Link>
+
+          <Link to="#"><span
+            className="inline-block text-white hover:text-red-500 my-2 w-full"
+          >
+            <span className="material-icons-outlined float-left pr-2">
+              email
+            </span>
+            Newsletters
+          </span>
+          </Link>
+          <span className="text-gray-500 text-sm">Main menu</span>
+          <Link to="#"><span
+            className="inline-block text-white hover:text-red-500 my-2 w-full"
+          >
+            <span className="material-icons-outlined float-left pr-2">
+              face
+            </span>
+            Accounts Manager
+          </span>
+          </Link>
+
+          <Link to="#"><span
+            onClick={handleLogout}
+            className="inline-block text-white hover:text-red-500 my-2 w-full hover:cursor-pointer"
+          >
+            <span className="material-icons-outlined float-left pr-2">
+              power_settings_new
+            </span>
+            Log out
+          </span>
+          </Link>
+        </div>
+      </nav>
+    </>
+  );
+};
--- client/src/admin-pages/Dashboard.js
+++ client/src/admin-pages/Dashboard.js
@@ -1,14 +1,12 @@
 import axios from "axios";
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import Logo from "../assets/Logo/logo-main.png";
+import Logo from "../assets/Logo/icon.png";
+import { SidebarNav } from "../admin-components/Navigation/SidebarNav";
 
 export const Dashboard = () => {
   const [sidebarOpen, setSidebarOpen] = useState(false); //This will be the declarations for toggle of sidebar.
-  const storedId = sessionStorage.getItem("session_id"); //Get the session id from the session storage
-  const navigate = useNavigate(); //Navigate to a specific path
 
-  //Sidebar toggle action
   const toggleSidebar = () => {
     setSidebarOpen(!sidebarOpen);
   };
@@ -17,19 +15,16 @@ export const Dashboard = () => {
     const handleResize = () => {
       setSidebarOpen(window.innerWidth >= 720); // Adjust the breakpoint as needed
     };
-
     window.addEventListener("resize", handleResize);
     handleResize(); // Set the initial state based on window width
-
     return () => {
       window.removeEventListener("resize", handleResize);
     };
   }, []);
 
-  const handleLogout = () => {
-    sessionStorage.removeItem("session_id");
-    navigate("/admin");
-  };
+  const storedId = sessionStorage.getItem("session_id"); //Get the session id from the session storage
+  const navigate = useNavigate(); //Navigate to a specific path
+
 
   useEffect(() => {
     axios
@@ -37,11 +32,9 @@ export const Dashboard = () => {
       .then((response) => {
         const data = response.data.data;
         const matchedData = data.filter((item) => item._id === storedId);
-        // console.log(matchedData);
         if (matchedData.length === 0) {
           navigate("/admin");
         } else {
-          // Process the matchedData or do something with it
         }
       })
       .catch((error) => {
@@ -58,37 +51,17 @@ export const Dashboard = () => {
           } flex flex-col w-64 bg-gray-800`}
         >
           <div className="flex items-center justify-center h-16 bg-gray-900">
-            {/* <span className="text-white font-bold uppercase">Bay-Ler View</span> */}
-            <img src={Logo} alt="" className="admin-logo"/>
+            <img src={Logo} alt="" className="admin-logo" />
+            <span className="text-white font-bold uppercase">
+              Bay-Ler View Admin
+            </span>
           </div>
           <div className="flex flex-col flex-1 overflow-y-auto">
-            <nav className="flex-1 px-2 py-4 bg-gray-800">
-              <span
-                className="flex items-center px-4 py-2 text-gray-100 hover:bg-gray-700"
-                onClick={toggleSidebar}
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6 mr-2"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M4 6h16M4 12h16M4 18h16"
-                  />
-                </svg>
-                Dashboard
-              </span>
-            </nav>
+            <SidebarNav />
           </div>
         </div>
 
-        {/* Main content */}
-        <div className="flex flex-col flex-1 overflow-y-auto">
+        <div className="flex flex-col flex-1 overflow-y-auto bg-red-100">
           <div className="flex items-center justify-between h-16 bg-white border-b border-gray-200">
             <div className="flex items-center px-4">
               <button
@@ -113,16 +86,9 @@ export const Dashboard = () => {
             </div>
             <div className="flex items-center pr-4">
               <button
-                onClick={handleLogout}
-                className="mx-2 flex items-center text-gray-500 hover:text-gray-700 focus:outline-none focus:text-gray-700"
-              >
-                Profile
-              </button>
-              <button
-                onClick={handleLogout}
                 className="flex items-center text-gray-500 hover:text-gray-700 focus:outline-none focus:text-gray-700"
               >
-                Logout
+                Hi, Richard
               </button>
             </div>
           </div>
